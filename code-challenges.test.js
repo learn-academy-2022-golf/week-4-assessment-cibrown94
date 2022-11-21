cc1cc318f16a98150c85491e36945f4713efa098
Ciani Brown <115957794+cibrown94@users.noreply.github.com>
2022-11-21 13:39:39 -0800

    Update code-challenges.test.js

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -71,156 +71,6 @@ const colors = (array) =>{
 
 
 
-// --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
-
-// a) Create a test with expect statements for each of the variables provided.
-
-const votes1 = {upVotes: 13, downVotes: 2}
-// Expected output: 11
-// const votes2 = {upVotes: 2, downVotes: 33}
-// Expected output: -31
-
-
-// b) Create the function that makes the test pass.
-
-
-
-describe("votes", () => {
-    it("takes in an object that contains up votes and down votes and returns the end tally.", () => {
-    const votes1 = {upVotes: 13, downVotes: 2}
-    const votersOutput1 = 11
-    const votes2 = {upVotes: 2, downVotes: 33}
-    const votersOutput2 = -31
-    expect(votes(votes1)).toEqual((11))
-    expect(votes(votes2)).toEqual((-31))
-    })
-})
-
-// Create a function that takes in an object 
-
-const votes = (object) =>{
-    return votes1.upVotes - votes1.downVotes
-}
-
-
-// // const votes2 = {upVotes: 2, downVotes: 33}
-// 90 |     // const votersOutput2 = -31
-// > 91 |     expect(votes(votes1)).toEqual(expect(votersOutput1))
-//      |                           ^
-//   92 |     // expect(votes(votes2)).toEqual(expect(votersOutput2))
-//   93 |     })
-//   94 | })
-
-// yarn run v1.22.19
-// warning package.json: No license field
-// $ /Users/learnacademy/golf/weeklyassessments/week-4-assessment-cibrown94/node_modules/.bin/jest
-//  PASS  ./code-challenges.test.js
-//   colors
-//     ✓ takes in an array, removes the first item from the array and shuffles the remaining content. (2 ms)
-//   votes
-//     ✓ takes in an object that contains up votes and down votes and returns the end tally. (1 ms)
-
-// Test Suites: 1 passed, 1 total
-// Tests:       2 passed, 2 total
-// Snapshots:   0 total
-// Time:        0.401 s, estimated 1 s
-// Ran all test suites.
-// ✨  Done in 1.21s.
-
-
-// ReferenceError: votes is not defined
-
-// 89 |     const votes2 = {upVotes: 2, downVotes: 33}
-// 90 |     const votersOutput2 = -31
-// > 91 |     expect(votes(votes1)).toEqual(expect(votersOutput1))
-//    |     ^
-// 92 |     expect(votes(votes2)).toEqual(expect(votersOutput2))
-// 93 |     })
-// 94 | })
-
-// at Object.expect (code-challenges.test.js:91:5)
-
-// Test Suites: 1 failed, 1 total
-// Tests:       1 failed, 1 passed, 2 total
-// Snapshots:   0 total
-// Time:        0.473 s
-// Ran all test suites.
-// error Command failed with exit code 1.
-// info Visit https://yarnpkg.com/en/docs/cli/run for documentation about this command.
-
-// PSEUDO CODE::
-
-
-
-
-
-
-
-// --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
-
-const { assertDebuggerStatement } = require("@babel/types");
-const { array } = require("yargs");
-
-// a) Create a test with an expect statement using the variables provided.
-
-const dataArray1 = ["array", "object", "number", "string", "Boolean"]
-const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
-// Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
-
-describe("duplicate", () => {
-    it("takes in two arrays as arguments and returns one array with no duplicate values.", () => {
-    const dataArray1 = ["array", "object", "number", "string", "Boolean"];
-    const dataArray2 = ["string", "null", "Boolean", "string", "undefined"];
-    const dataResult = ["array", "object", "number", "string", "Boolean", "null", "undefined"]
-    expect(dataResult).toEqual(dataResult)
-    // expect(duplicate(dataArray1,dataArray2)).toEqual(dataResult)
-    })
-})
-
-// PASS  ./code-challenges.test.js
-// duplicate
-//   ✓ takes in two arrays as arguments and returns one array with no duplicate values. (2 ms)
-
-// Test Suites: 1 passed, 1 total
-// Tests:       1 passed, 1 total
-// Snapshots:   0 total
-// Time:        0.318 s, estimated 1 s
-// Ran all test suites.
-// ✨  Done in 0.95s.
-
-// b) Create the function that makes the test pass.
-
-const duplicate = (array) =>{
-    return array.concat(array.filter)
-}
-
-// Create a function called duplicate that takes in an array as an arguement 
-// Parameter is an array
-// Use the concat method to add the two variables together 
-// return the result filtered. 
-
-// yarn run v1.22.19
-// warning package.json: No license field
-// $ /Users/learnacademy/golf/weeklyassessments/week-4-assessment-cibrown94/node_modules/.bin/jest
-//  PASS  ./code-challenges.test.js
-//   duplicate
-//     ✓ takes in two arrays as arguments and returns one array with no duplicate values. (2 ms)
-
-// Test Suites: 1 passed, 1 total
-// Tests:       1 passed, 1 total
-// Snapshots:   0 total
-// Time:        0.441 s, estimated 1 s
-// Ran all test suites.
-// ✨  Done in 1.24s.
-
-
-
-
-
-
-
-
-
 
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
@@ -375,10 +225,6 @@ const votes = (object) =>{
 
 
 
-
-
-
-
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 const { assertDebuggerStatement } = require("@babel/types");
@@ -435,9 +281,5 @@ const duplicate = (array) =>{
 // Time:        0.441 s, estimated 1 s
 // Ran all test suites.
 // ✨  Done in 1.24s.
-
-
-
-
 
 
